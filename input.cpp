f5f1f0ab31673a42d1e3e0ee0daf0191f1a6553d
Dzaki Afrian <60803453+Dz-Aplian-333@users.noreply.github.com>
2025-01-17 22:45:02 +0700

    cara input keyboard

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama;
+    cout << "Tuliskan nama: ";
+    cin >> nama;
+
+    cout << "Hi " << nama;
+    cout << " selamat datang diKelas A";
+    
+    return 0;
+}
